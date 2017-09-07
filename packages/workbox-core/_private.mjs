0718d650224d446e62dc288222c643a30f5cd774
Matt Gaunt <matt@gauntface.co.uk>
2017-09-07 11:31:40 -0700

    # This is a combination of 2 commits. # This is the 1st commit message:
    Moving to an _private method
    
    Adding changes from feedback from PR
    
    # This is the commit message #2:
    
    Fixing lint

--- packages/workbox-core/_private.mjs
+++ packages/workbox-core/_private.mjs
@@ -1,12 +1,7 @@
 import logger from './utils/logger.mjs';
 import WorkboxError from './models/WorkboxError.mjs';
 
-<<<<<<< HEAD
 export {
   logger,
   WorkboxError,
 };
-=======
-export {logger};
-export {WorkboxError};
->>>>>>> 92d113eb... Moving to an _private method
