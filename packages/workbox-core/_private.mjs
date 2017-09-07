7a1719c96b2dc34de81aa6198ba23a11bf5dd13b
Matt Gaunt <matt@gauntface.co.uk>
2017-09-07 11:30:56 -0700

    Moving to an _private method

--- packages/workbox-core/_private.mjs
+++ packages/workbox-core/_private.mjs
@@ -1,7 +1,12 @@
 import logger from './utils/logger.mjs';
 import WorkboxError from './models/WorkboxError.mjs';
 
+<<<<<<< HEAD
 export {
   logger,
   WorkboxError,
 };
+=======
+export {logger};
+export {WorkboxError};
+>>>>>>> 92d113eb... Moving to an _private method
